a9b18a18fd934ea7bbbe005056491acb337c7f7b
Darren Hoehna <dahoehna@microsoft.com>
2019-11-01 13:35:25 -0700

    Removing a test that assumed path redirection would be the same for a known path and it's path in a package

--- tests/scenarios/FileSystemTest/EnumerateFilesTest.cpp
+++ tests/scenarios/FileSystemTest/EnumerateFilesTest.cpp
@@ -75,6 +75,9 @@ static int EnumerateFilesTest(const vfs_mapping& mapping)
             ::CloseHandle(file);
             return ERROR_SUCCESS;
         };
+
+		//COmmenting the rest out since writing to the known path and the package path
+		//results in different redirected paths.
         result = createFile(path, L"á.txt"sv);
         if (result) return result;
         result = createFile(packagePath, L"β.txt"sv);
@@ -85,18 +88,6 @@ static int EnumerateFilesTest(const vfs_mapping& mapping)
         if (result) return result;
         auto newPackageFiles = doEnumerate(packagePath, result);
         if (result) return result;
-
-        // NOTE: The non-fixed version of this function has access to some of the folders (e.g. Common AppData), so
-        //       we may hit spurious failures if we try and run the fixed test after the non-fixed test, so try and
-        //       delete the files. This is best effort
-        ::DeleteFileW((path / L"á.txt"sv).c_str());
-        ::DeleteFileW((packagePath / L"β.txt"sv).c_str());
-
-        if ((newFiles.size() != files.size() + 2) || (newPackageFiles.size() != packageFiles.size() + 2))
-        {
-            trace_message(L"ERROR: Did not find the newly created files\n", error_color);
-            return ERROR_ASSERTION_FAILURE;
-        }
 
         if (!std::includes(newFiles.begin(), newFiles.end(), newPackageFiles.begin(), newPackageFiles.end()))
         {
