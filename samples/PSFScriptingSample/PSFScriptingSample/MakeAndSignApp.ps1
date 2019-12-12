9bd39f96ff4db58784da62dbe19498d2342fffdf
Darren Hoehna <dahoehna@microsoft.com>
2019-12-12 14:15:08 -0800

    script search for the most recent windows kit to use

--- samples/PSFScriptingSample/PSFScriptingSample/MakeAndSignApp.ps1
+++ samples/PSFScriptingSample/PSFScriptingSample/MakeAndSignApp.ps1
@@ -1,2 +1,17 @@
-﻿./GenerateMSIX.cmd "bin\release" "bin\release\PSFScriptingSample"
-./SignMSIX.cmd "bin\release\PSFScriptingSample.msix"
+﻿$windowKits = get-childitem "C:\Program Files (x86)\Windows Kits\10\bin" -filter "10.0*" | sort-object -property name -descending
+
+$foundAWindowKit = $false
+$windowKitIndex = 0
+while(-not $foundAWindowKit -and $windowKitIndex -le $windowKits.count)
+{
+	if(test-path($windowKits[$windowKitIndex].fullName + "\x64"))
+	{
+		$env:Path += (";" + $windowKits[$windowKitIndex] + "\x64")
+		$foundAWindowKit = $true
+	}
+
+	$windowKitIndex++
+}
+
+makeappx pack /d bin\release /p bin\release\PSFScriptingSample.msix
+signtool sign /p "Password12$" /a /v /fd SHA256 /f "bin\release\PSFScriptingSample_TemporaryKey.pfx" "bin\release\PSFScriptingSample.msix"
