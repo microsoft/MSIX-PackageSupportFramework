5848c0b2c8997ceb713db191d3ad117c22844bb9
Tim Mangan <tim@tmurgent.onmicrosoft.com>
2020-06-05 06:13:38 -0400

    Added additional output to aid in debugging tests.

--- buildall.cmd
+++ buildall.cmd
@@ -1,11 +1,19 @@
 @echo off
+echo "==================== PSF x86  DEBUG  ================================="
 msbuild CentennialFixups.sln /p:platform=x86;configuration=debug
+echo "==================== PSF x86 RELEASE ================================="
 msbuild CentennialFixups.sln /p:platform=x86;configuration=release
+echo "==================== PSF x64  DEBUG  ================================="
 msbuild CentennialFixups.sln /p:platform=x64;configuration=debug
+echo "==================== PSF x64 RELEASE ================================="
 msbuild CentennialFixups.sln /p:platform=x64;configuration=release
 pushd tests
+echo "==================== TEST x86  DEBUG  ================================="
 msbuild tests.sln /p:platform=x86;configuration=debug
+echo "==================== TEST x86 RELEASE ================================="
 msbuild tests.sln /p:platform=x86;configuration=release
+echo "==================== TEST x64  DEBUG  ================================="
 msbuild tests.sln /p:platform=x64;configuration=debug
+echo "==================== TEST x64 RELEASE ================================="
 msbuild tests.sln /p:platform=x64;configuration=release
 popd
