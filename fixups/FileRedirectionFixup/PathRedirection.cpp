9bb5dc060a7467d0a20f622e326369a445cdbcb6
Tim Mangan <tim@tmurgent.onmicrosoft.com>
2021-02-18 11:52:23 -0500

    Fix redundancy check.

--- fixups/FileRedirectionFixup/PathRedirection.cpp
+++ fixups/FileRedirectionFixup/PathRedirection.cpp
@@ -1202,8 +1202,9 @@ static path_redirect_info ShouldRedirectImpl(const CharT* path, redirect_flags f
     LogString(inst, L"\tFRF Should: for path", widen(path).c_str());
     
     size_t found = (widen(path)).find(L"WritablePackageRoot", 0);
-    if (found != 0)
+    if (found != std::wstring::npos)
     {
+        // We do not want to redirect something already in the redirect area...
         LogString(inst, L"Prevent redundant redirection.", widen(path).c_str());
         return result;
     }
